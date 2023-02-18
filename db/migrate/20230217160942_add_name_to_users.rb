f3b818f95bd6ce858c22928f3a07165d6bb6f308
Mykola Litynskyi <mklins-dev@proton.me>
2023-02-18 15:47:50 +0200

    Added Devise and User model, configured it all and added basic home page

---
+++ db/migrate/20230217160942_add_name_to_users.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class AddNameToUsers < ActiveRecord::Migration[7.0]
+  def up
+    add_column :users, :name, :string
+  end
+
+  def down
+    remove_column :users, :name
+  end
+end
