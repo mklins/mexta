f56f3109771b9c55ab1cc92f1863f1f60d927400
Mykola Litynskyi <94116748+mklins@users.noreply.github.com>
2023-02-27 08:11:11 +0200

    Created home page (#11)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!, except: :home
+  before_action :authenticate_user!
 
   def after_sign_in_path_for(_resource)
     spendings_path
